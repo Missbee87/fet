7f58435c1714343f8e08114b147162035c7fe220
Belen <blnmolina@gmail.com>
2022-03-26 18:23:57 -0300

    cambios

--- js/app1.js
+++ js/app1.js
@@ -1,6 +1,26 @@
-const search = document.querySelector("#busqueda");
+let links = document.querySelectorAll("nav a");
+console.log(links);
+
+for (let i = 0; i < links.length; i += 1) {
+links[i].addEventListener("click", pedirPagina);
+}
+
+function pedirPagina(e) {
+    e.preventDefault();
+    console.log(e.target.dataset.pagina);
+    let url = `${e.target.dataset.pagina}.html`;
+    console.log(url);
 
 
-search.addEventListener("input", () => {
-    console.log("escribiendo...");
-});
+
+    const miPagina = document.querySelector("#miPagina");
+    fetch(url).then((pagina) => {
+        console.log(pagina);
+        pagina.text().then((page) => {
+        console.log(page);
+        miPagina.innerHTML = page;
+
+    })
+    
+    })
+}
